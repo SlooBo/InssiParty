96728fb338bd9e2177815923ce785eb2178e27b2
Markus Tolvanen <markustolvanen@wippies.fi>
2013-11-07 16:18:11 +0200

    Runkoa

---
+++ InssiParty/InssiParty/Games/Olut.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InssiParty.Engine;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace InssiParty.Games
+{
+
+    /**
+     * Uuden pelin luominen
+     * 
+     * -> kopio tämä filu, ja nimeä se ja classin nimi uusiksi
+     * -> lisää listaan InssiGame.cs tiedostossa
+     * 
+     */
+
+    /**
+     * Classin nimi pitää vaihtaa, mieluiten sama kuin tiedoston nimi.
+     * IGameBase pitää jättää semmoiseksi
+     */
+
+    /**
+     * PelinNimi
+     * 
+     * Selitys pelistä
+     * 
+     * By: Tekijän Nimi
+     */
+    class Olut : GameBase
+    {
+        //Mahdolliset variablet mitä tarvitset pelin aikana on hyvä listata tässä kohdassa.
+        private int value;
+        private string [] numValues;
+        private int counter;
+
+
+        //Tekstuurit pitää myös listata tässä kohdassa.
+        private Texture2D Background;
+        private Texture2D Can;
+        private Texture2D HissingCan;
+        private Texture2D EmptyCan;
+
+        /**
+         * Lataa tekstuureihin itse data.
+         * 
+         * Ajetaan kun koko ohjelma käynnistyy.
+         */
+        public override void Load(ContentManager Content)
+        {
+            //Tiedoston pitäisi olla InssiPartyContent projektin alla solution explorerissa.
+            Background = Content.Load<Texture2D>("propelli");
+            Can = Content.Load<Texture2D>("propelli");
+            HissingCan = Content.Load<Texture2D>("propelli");
+            EmptyCan = Content.Load<Texture2D>("propelli");
+        }
+
+        /**
+         * Kaikki mitä pitää tehdä kun peli käynnistyy.
+         * 
+         * Esimerkiksi aseta muuttujat tarvittaviin arvoihin, tai käynnistä musiikki.
+         */
+        public override void Start()
+        {
+            Console.WriteLine("Starting Olut");
+
+            value = 500;
+        }
+
+        /**
+         * Ajetaan kun peli sulkeutuu. Piilota äänet ja puhdista roskasi seuraavaa peliä varten.
+         */
+        public override void Stop()
+        {
+            Console.WriteLine("Closing Olut");
+        }
+
+        /**
+         * Ajetaan kun peliä pitää päivittää. Tänne menee itse pelin logiikka koodi,
+         * törmäys chekkaukset, pisteen laskut, yms.
+         * 
+         * gameTime avulla voidaan synkata nopeus tasaikseksi vaikka framerate ei olisi tasainen.
+         */
+        public override void Update(GameTime gameTime)
+        {
+            
+
+            if (value < 0)
+            {
+                //sammuta peli, true jos voitto tapahtui, false jos pelaaaja hävisi.
+                CloseGame(true);
+            }
+        }
+
+        /**
+         * Pelkkä piirtäminen
+         * 
+         * ELÄ sijoita pelilogiikkaa tänne.
+         *
+         * gameTime avulla voidaan synkata nopeus tasaikseksi vaikka framerate ei olisi tasainen.
+         */
+        public override void Render(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            Console.WriteLine("Draw " + value);
+
+            //spriteBatch.Draw funktiolla voit piirtää ruudulle.
+            //Palikka piirretään y akselilla, valuen kohtaan
+            spriteBatch.Draw(Background, new Vector2(50, value), Color.White);
+            spriteBatch.Draw(Background, new Vector2(50, value), Color.White);
+            spriteBatch.Draw(Background, new Vector2(50, value), Color.White);
+            spriteBatch.Draw(Background, new Vector2(50, value), Color.White);
+        }
+
+    }
+}
