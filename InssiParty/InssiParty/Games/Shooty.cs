d514ddecbcc637342c1c6699b71e9c2ed69a9d2d
Althasandrian <Althasandrian@gmail.com>
2013-11-11 20:09:32 +0200

    Hiiren liike ja tekstuurit

--- InssiParty/InssiParty/Games/Shooty.cs
+++ InssiParty/InssiParty/Games/Shooty.cs
@@ -2,10 +2,118 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using InssiParty.Engine;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
 
 namespace InssiParty.Games
 {
-    class Shooty
+
+    /**
+     * Uuden pelin luominen
+     * 
+     * -> kopio tämä filu, ja nimeä se ja classin nimi uusiksi
+     * -> lisää listaan InssiGame.cs tiedostossa
+     * 
+     */
+
+    /**
+     * Classin nimi pitää vaihtaa, mieluiten sama kuin tiedoston nimi.
+     * IGameBase pitää jättää semmoiseksi
+     */
+
+    /**
+     * PelinNimi
+     * 
+     * Selitys pelistä
+     * 
+     * By: Tekijän Nimi
+     */
+    class Shooty : GameBase
     {
+        //Mahdolliset variablet mitä tarvitset pelin aikana on hyvä listata tässä kohdassa.
+        private int value, health;
+
+        Rectangle background = new Rectangle(0, 0, 800, 600);
+        Rectangle targetRect = new Rectangle(0, 0, 62, 62); 
+
+        private Vector2 targetPos;
+        //Tekstuurit pitää myös listata tässä kohdassa.
+        private Texture2D spriteBox;
+        private Texture2D targetTexture;
+        /**
+         * Lataa tekstuureihin itse data.
+         * 
+         * Ajetaan kun koko ohjelma käynnistyy.
+         */
+        public override void Load(ContentManager Content)
+        {
+            //Tiedoston pitäisi olla InssiPartyContent projektin alla solution explorerissa.
+            spriteBox = Content.Load<Texture2D>("propelli");
+            targetTexture = Content.Load<Texture2D>("Target");
+        }
+
+        /**
+         * Kaikki mitä pitää tehdä kun peli käynnistyy.
+         * 
+         * Esimerkiksi aseta muuttujat tarvittaviin arvoihin, tai käynnistä musiikki.
+         */
+        public override void Start()
+        {
+            Console.WriteLine("Starting hello world");
+
+            value = 500;
+        }
+
+        /**
+         * Ajetaan kun peli sulkeutuu. Piilota äänet ja puhdista roskasi seuraavaa peliä varten.
+         */
+        public override void Stop()
+        {
+            Console.WriteLine("Closing hello world");
+        }
+
+        /**
+         * Ajetaan kun peliä pitää päivittää. Tänne menee itse pelin logiikka koodi,
+         * törmäys chekkaukset, pisteen laskut, yms.
+         * 
+         * gameTime avulla voidaan synkata nopeus tasaikseksi vaikka framerate ei olisi tasainen.
+         */
+        public override void Update(GameTime gameTime)
+        {
+            value--;
+
+            var mouseState = Mouse.GetState();
+            targetRect.X = mouseState.X;
+            targetRect.Y = mouseState.Y;
+            targetPos = new Vector2(mouseState.X, mouseState.Y);
+
+
+            if (value < 0)
+            {
+                //sammuta peli, true jos voitto tapahtui, false jos pelaaaja hävisi.
+                CloseGame(true);
+            }
+        }
+
+        /**
+         * Pelkkä piirtäminen
+         * 
+         * ELÄ sijoita pelilogiikkaa tänne.
+         *
+         * gameTime avulla voidaan synkata nopeus tasaikseksi vaikka framerate ei olisi tasainen.
+         */
+        public override void Render(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            Console.WriteLine("Draw " + value);
+
+            //spriteBatch.Draw funktiolla voit piirtää ruudulle.
+            //Palikka piirretään y akselilla, valuen kohtaan
+            spriteBatch.Draw(spriteBox, new Vector2(50, value), Color.White);
+            spriteBatch.Draw(targetTexture, targetPos, Color.White);
+        }
+
     }
 }
