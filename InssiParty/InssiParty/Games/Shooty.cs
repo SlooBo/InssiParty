14cbb0c65058ed31b9bda978230f8a931c9824cd
Althasandrian <Althasandrian@gmail.com>
2013-11-13 20:32:55 +0200

    vektoreita, vektoreita kaikkialla!

--- InssiParty/InssiParty/Games/Shooty.cs
+++ InssiParty/InssiParty/Games/Shooty.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using InssiParty.Engine;
+using InssiParty.Games;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Input;
+
 
 namespace InssiParty.Games
 {
@@ -34,13 +36,18 @@ namespace InssiParty.Games
     class Shooty : GameBase
     {
         private int nyancat_pos, health;
+        private double angle, PI, initX, initY;
+        private float inifX, inifY;
 
         Rectangle background = new Rectangle(0, 0, 800, 600);
         Rectangle targetRect = new Rectangle(0, 0, 62, 62);
         Rectangle NyancatRect = new Rectangle(0, 0, 62, 37);
         //Rectangle turretBarrelRect = new Rectangle(0, 0, 62, 62); 
         private Vector2 targetPos;
-        //private Vector2 barrelhp;
+        private Vector2 barrelhp;
+        private Vector2 cbSpeed;
+        private Vector2 cbPos;
+        private Vector2 tri;
 
         private Texture2D Nyancat;
         private Texture2D nyantail;
@@ -49,6 +56,8 @@ namespace InssiParty.Games
         private Texture2D backgroundTexture;
         private Texture2D turretTexture;
         //private Texture2D turretBarrelTexture;
+
+        List<Cannonball> cannonballs = new List<Cannonball>();
 
         public override void Load(ContentManager Content)
         {
@@ -71,6 +80,8 @@ namespace InssiParty.Games
         {
             nyancat_pos = 0;
             health = 3;
+            barrelhp = new Vector2(84, 418);
+            PI = 3.14159;
         }
 
         /**
@@ -89,12 +100,24 @@ namespace InssiParty.Games
          */
         public override void Update(GameTime gameTime)
         {
-            nyancat_pos+=2;
+
+            //nyancat_pos+=2;
+            // NYANI ON PYSÄYTETTY TESTAUKSIA VARTEN
 
             var mouseState = Mouse.GetState();
             targetRect.X = mouseState.X;
             targetRect.Y = mouseState.Y;
             targetPos = new Vector2(mouseState.X-32, mouseState.Y-32);
+
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                angle = Math.Atan((barrelhp.Y - mouseState.Y) / (mouseState.X - barrelhp.X)) * 180 / PI;
+                initX = Math.Cos(angle) * 6;
+                initY = Math.Sin(angle) * 6;
+                cbSpeed = new Vector2((float)initX, (float)initY);
+                cannonballs.Add(new Cannonball(cannonballTexture, barrelhp, cbSpeed));
+                Console.WriteLine(angle);
+            }
 
 
             if (nyancat_pos > background.Width)
@@ -122,6 +145,7 @@ namespace InssiParty.Games
             //spriteBatch.Draw funktiolla voit piirtää ruudulle.
             //Palikka piirretään y akselilla, valuen kohtaan
             spriteBatch.Draw(backgroundTexture, background, Color.White);
+            spriteBatch.Draw(cannonballTexture, cbPos, Color.White);
             spriteBatch.Draw(turretTexture, new Vector2(50, 400), Color.White);
             spriteBatch.Draw(nyantail, new Vector2(nyancat_pos - 187, 10), Color.White);
             spriteBatch.Draw(Nyancat, new Vector2(nyancat_pos, 10), Color.White);
---
+++ InssiParty/InssiParty/Objects/Cannonball.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace InssiParty.Games
+{
+    public class Cannonball
+    {
+        protected Texture2D _texture;
+
+        protected Vector2 _position;
+        protected Vector2 _speed;
+
+        public Vector2 Position { get { return _position; } }
+        public Rectangle CollisionRect { get; private set; }
+        public bool IsDead;
+
+        public Cannonball(Texture2D texture, Vector2 position, Vector2 speed)
+        {
+            _texture = texture;
+            _position = position;
+            _speed = speed;
+        }
+
+        public void Update()
+        {
+            CollisionRect = new Rectangle((int)_position.X,(int)_position.Y,
+                _texture.Bounds.Width, _texture.Bounds.Height);
+            _position += _speed;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _position, Color.White);
+        }
+    }
+}
