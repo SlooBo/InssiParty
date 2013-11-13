21ed33fad4f635c6b8b212099d984effaa42f8c3
Toucheaun <toucheaun@gmail.com>
2013-11-13 23:36:06 +0200

    koodia lisätty, ei toimi vielä oikein

--- InssiParty/InssiParty/Games/Lampunvaihto.cs
+++ InssiParty/InssiParty/Games/Lampunvaihto.cs
@@ -6,6 +6,7 @@ using InssiParty.Engine;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
 
 namespace InssiParty.Games
 {
@@ -21,9 +22,15 @@ namespace InssiParty.Games
     {
         //Variablet
         private int value;
-        Rectangle Lamppu = new Rectangle(300,180,180,180);
-        Rectangle Katkaisin = new Rectangle(530, 285, 560, 315);
-        Rectangle Uusilamppu = new Rectangle(710,500,770,550);
+        private int add = 0;
+        private int power = 0;
+        private Vector2 cursorPos;
+        Rectangle cursorRect = new Rectangle(0, 0, 1, 1);
+        Rectangle Lamppu = new Rectangle(300, 180, 180, 180);
+        Rectangle Katkaisin = new Rectangle(530, 285, 30, 30);
+        Rectangle Uusilamppu = new Rectangle(710, 500, 60, 50);
+        Rectangle Hollilla = new Rectangle(300, 240, 180, 180);
+
 
         //Tekstuurit
         private Texture2D Lamppupaalla;
@@ -32,6 +39,7 @@ namespace InssiParty.Games
         private Texture2D Lamppuvoitto;
         private Texture2D Lamppuboom;
         private Texture2D Lamppuaika;
+        private Texture2D Kursori;
 
         //Load content
         public override void Load(ContentManager Content, GraphicsDevice GraphicsDevice)
@@ -43,6 +51,7 @@ namespace InssiParty.Games
             Lamppuvoitto = Content.Load<Texture2D>("lamppuvoitto");
             Lamppuboom = Content.Load<Texture2D>("lamppuboom");
             Lamppuaika = Content.Load<Texture2D>("lamppuaika");
+            Kursori = Content.Load<Texture2D>("Lamppukursori");
         }
 
         /**
@@ -53,9 +62,6 @@ namespace InssiParty.Games
         public override void Start()
         {
             Console.WriteLine("Starting Lampun vaihto");
-
-            
-            
         }
 
         /**
@@ -74,7 +80,74 @@ namespace InssiParty.Games
          */
         public override void Update(GameTime gameTime)
         {
-           
+            var mouseState = Mouse.GetState();
+            cursorRect.X = mouseState.X;
+            cursorRect.Y = mouseState.Y;
+            cursorPos = new Vector2(mouseState.X, mouseState.Y);
+
+            //Otetaan lamppu irti
+            if (add < 20)
+            {
+                Console.WriteLine("Lamppu on himmea");
+
+                if (Lamppu.Intersects(cursorRect))
+                {
+                    Console.WriteLine("Pitäisikö se vaihtaa?");
+
+                    if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+                    {
+                        Console.WriteLine("Hailuta niinku hullu!");
+                        if (Lamppu.Intersects(cursorRect))
+                        {
+                            add++;
+                            Console.WriteLine(add);
+                        }
+                    }
+
+                }
+                if (Katkaisin.Intersects(cursorRect))
+                {
+                    Console.WriteLine("Katkaisin");
+                    if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+                    {
+                        power = 1;
+                        Console.WriteLine("Virta on pois");
+                    }
+                }
+            }
+
+            //Laitetaan toinen lamppu hollille
+            else if (add > 20)
+            {
+                Lamppupaalla = Lamppupois;
+                Console.WriteLine("Tarvitaan uusi lamppu");
+
+                if (Uusilamppu.Intersects(cursorRect))
+                {
+                    Console.WriteLine("Klikkaa uusi lamppu kayttoon");
+                    if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+                    {
+                        Lamppupois = Lamppuhollilla;
+
+                        //Lamppu on hollilla, tässä pitää viimeistään muistaa ottaa virrat pois
+                        if (Katkaisin.Intersects(cursorRect))
+                        {
+                            Console.WriteLine("Katkaisin");
+
+                            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+                            {
+                                power = 1;
+                                Console.WriteLine("Virta on pois");
+                            }
+                        }
+
+                        if (Hollilla.Intersects(cursorRect))
+                        {
+
+                        }
+                    }
+                }
+            }
         }
 
         /**
@@ -86,11 +159,12 @@ namespace InssiParty.Games
          */
         public override void Render(SpriteBatch spriteBatch, GameTime gameTime)
         {
-            
+
 
             //spriteBatch.Draw funktiolla voit piirtää ruudulle.
             //Palikka piirretään y akselilla, valuen kohtaan
-            spriteBatch.Draw(Lamppupaalla, new Vector2(0,0), Color.White);
+            spriteBatch.Draw(Lamppupaalla, new Vector2(0, 0), Color.White);
+            spriteBatch.Draw(Kursori, cursorPos, Color.White);
         }
 
     }
