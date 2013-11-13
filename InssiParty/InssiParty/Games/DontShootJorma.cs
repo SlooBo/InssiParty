695f1252738f1c0ccc0c2f186e51e363e4fb60a3
Lauri Mäkinen <lauri.sakari.makinen@gmail.com>
2013-11-13 18:59:12 +0200

    Jorman tapon logiikan parantelua

--- InssiParty/InssiParty/Games/DontShootJorma.cs
+++ InssiParty/InssiParty/Games/DontShootJorma.cs
@@ -23,12 +23,19 @@ namespace InssiParty.Games
      */
     class DontShootJorma : GameBase
     {
+        /* Resources */
         private Texture2D jormaTexture;
         private Texture2D shotgun1Texture;
         private Texture2D shotgun2Texture;
         private Texture2D cursorTexture;
 
+        private SpriteFont font;
+
+        /* Variables */
         private Vector2 cursorPosition;
+
+        private int timer;
+        private bool shotDone;
 
         public override void Load(ContentManager Content)
         {
@@ -37,12 +44,15 @@ namespace InssiParty.Games
             shotgun2Texture = Content.Load<Texture2D>("dontShoot/shotgun2");
             cursorTexture = Content.Load<Texture2D>("k00panhiiri");
 
+            font = Content.Load<SpriteFont>("DefaultFont");
+
             cursorPosition = new Vector2(0, 0);
         }
 
         public override void Start()
         {
-            //TODO: reset timers
+            timer = 0;
+            shotDone = false;
         }
 
         public override void Stop() { }
@@ -53,17 +63,38 @@ namespace InssiParty.Games
             cursorPosition.Y = Mouse.GetState().Y;
 
             //TODO: Do a check for the game start so the player wont shoot by starting the game.
-            if (Mouse.GetState().RightButton == ButtonState.Pressed)
+            if (Mouse.GetState().RightButton == ButtonState.Pressed || Mouse.GetState().LeftButton == ButtonState.Pressed)
             {
-                //TODO: Add timer + particles
-                CloseGame(false);
+                if (timer > 100)  //Prevent click on the start menu
+                {
+                    shotDone = true;
+                }
             }
+
+            ++timer;
         }
 
         public override void Render(SpriteBatch spriteBatch, GameTime gameTime)
         {
-            spriteBatch.Draw(shotgun1Texture, new Vector2(320, 463), Color.WhiteSmoke);
-            spriteBatch.Draw(cursorTexture, cursorPosition, Color.White);
+            if (timer < 100)
+            {
+                //Show the guide on the start
+                spriteBatch.DrawString(font, "Don't Shoot jorma.", new Vector2(300, 280), Color.Red);
+            }
+            else
+            {
+
+                if (shotDone == false)
+                {
+                    spriteBatch.Draw(shotgun1Texture, new Vector2(320, 463), Color.WhiteSmoke);
+                }
+                else
+                {
+                    spriteBatch.Draw(shotgun2Texture, new Vector2(320, 463), Color.WhiteSmoke);
+                }
+
+                spriteBatch.Draw(cursorTexture, cursorPosition, Color.White);
+            }
         }
 
     }
