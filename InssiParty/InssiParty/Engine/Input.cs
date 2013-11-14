d549b74c6aede3021bdca35da532e85da6172db3
Althasandrian <Althasandrian@gmail.com>
2013-11-14 14:50:22 +0200

    Added Input.cs to Engine folder
    get keystates from here

---
+++ InssiParty/InssiParty/Engine/Input.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace CodeClubShmup1.Engine
+{
+    class Input
+    {
+        static KeyboardState k_state, k_state_old;
+
+        public static void UpdateState()
+        {
+            k_state_old = k_state;
+            k_state = Keyboard.GetState();
+        }
+
+        public static bool IsKeyDown(Keys key)
+        {
+            return k_state.IsKeyDown(key);
+        }
+
+        public static bool IsKeyUp(Keys key)
+        {
+            return k_state.IsKeyUp(key);
+        }
+
+        public static bool IsKeyPressed(Keys key)
+        {
+            return k_state.IsKeyDown(key) && k_state_old.IsKeyUp(key);
+        }
+
+        public static bool IsKeyReleased(Keys key)
+        {
+            return k_state.IsKeyUp(key) && k_state_old.IsKeyDown(key);
+        }
+
+    }
+}
