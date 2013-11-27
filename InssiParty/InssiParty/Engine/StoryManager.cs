626dd02002bd9c25876cf4aebc7da3b087576548
Lauri Mäkinen <lauri.sakari.makinen@gmail.com>
2013-11-27 19:34:33 +0200

    Randomizing the next game and preventing playing same twice.

--- InssiParty/InssiParty/Engine/StoryManager.cs
+++ InssiParty/InssiParty/Engine/StoryManager.cs
@@ -7,6 +7,9 @@ namespace InssiParty.Engine
 {
     /**
      * Manages the story state!
+     * 
+     * Starts new games when needed, tells when the games should be stopped.
+     * Keeps count what games have been played and whats the current score.
      */
     class StoryManager
     {
@@ -14,11 +17,20 @@ namespace InssiParty.Engine
         public int points { get; set; }
 
         private List<GameBase> games;
+        private List<GameBase> gamesPlayed;
+
+        private Random random;
 
         public StoryManager(List<GameBase> games)
         {
             //Get the pointer to the game list.
             this.games = games;
+
+            //Games list is just a local list of the games played so same game won't come up twice.
+            gamesPlayed = new List<GameBase>();
+
+            random = new Random();
+
             Console.WriteLine("[StoryManager] Init ok!");
         }
 
@@ -32,9 +44,16 @@ namespace InssiParty.Engine
             Console.WriteLine("[StoryManager] Starting story...");
             points = 0;
             hp = 0;
+
+            StartNextGame();
         }
 
         public void StoryLogicUpdate()
+        {
+            //TODO: Move the game updating and drawing here ?!?
+        }
+
+        public void StoryLogicDraw()
         {
         }
 
@@ -52,6 +71,41 @@ namespace InssiParty.Engine
             }
 
             //TODO: Randomize next game.
+        }
+
+        private void StartNextGame()
+        {
+            bool gameSelected = false;
+            int id = 0;
+            int missCount = -1;
+
+            if(gamesPlayed.Count == games.Count)
+            {
+                //All games have been played! Give extra +5 points and clear the list.
+                points += 5;
+
+                gamesPlayed.Clear();
+                Console.WriteLine("[StoryManager] All games played! +5 points and clearing list");
+            }
+
+            while (gameSelected == false)
+            {
+                id = random.Next(0, games.Count);
+                gameSelected = true;
+
+                //check if it has been already played
+                for (int i = 0; i < gamesPlayed.Count; ++i)
+                {
+                    if (games[id] == gamesPlayed[i])
+                        gameSelected = false; //The game was already played.
+                }
+
+                ++missCount;
+            }
+
+            Console.WriteLine("[StoryManager] " + games[id].Name + " chosen. (" + missCount + ") miss count on randoming.");
+
+            gamesPlayed.Add(games[id]);
         }
     }
 }
